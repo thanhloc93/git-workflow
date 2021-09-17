bb0434c6a683303b34fe9f92c52108c9768e7ad1
thanhloc93 <thanhloc.nguyen@lixi.vn>
2021-09-17 22:32:40 +0700

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Nguyen thanh loc 1
+          Nguyen thanh Lộc
         </p>
         <a
           className="App-link"
