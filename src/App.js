7f2988beac7f772c0e937acb4788c6a09f52ed46
thanhloc93 <thanhloc.nguyen@lixi.vn>
2021-09-17 23:03:19 +0700

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Vk iu 1
+          32132131
         </p>
         <a
           className="App-link"
