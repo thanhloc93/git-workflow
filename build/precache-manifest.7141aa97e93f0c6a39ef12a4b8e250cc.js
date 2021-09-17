eb35dee6cd09ba4c6b2a4e1c44951438fabcc24e
thanhloc93 <thanhloc.nguyen@lixi.vn>
2021-09-17 22:46:32 +0700

    update

---
+++ build/precache-manifest.7141aa97e93f0c6a39ef12a4b8e250cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fbe8906496de8e2f86f7ae2b06a296f",
+    "url": "/git-workflow/index.html"
+  },
+  {
+    "revision": "2bac71257ed85910bbe2",
+    "url": "/git-workflow/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "acd16fc72ce131a504a3",
+    "url": "/git-workflow/static/js/2.6b493f2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git-workflow/static/js/2.6b493f2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bac71257ed85910bbe2",
+    "url": "/git-workflow/static/js/main.a3869ca1.chunk.js"
+  },
+  {
+    "revision": "d890ba64377c243bbe51",
+    "url": "/git-workflow/static/js/runtime-main.5fad8ac9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/git-workflow/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Nguyen thanh Lộc
+          Nguyen thanh Lộc 1
         </p>
         <a
           className="App-link"
