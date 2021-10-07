1c12e107f3f7e435d749a388b2ac1e968453793d
Thanhloc <cm.thanhloc@gmail.com>
2021-10-07 13:55:08 +0700

    [LT-7777] xin chao

---
+++ .githooks/prepare-commit-msg.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# get ticketid from branch name
+regexp_message="(\b(LT|lt))-([0-9]{1,6})"
+get_id() {
+    MESSAGE="$1"
+    CURRENT_BRANCH="$2"
+    get_ticket_id() {
+        regexp_message="(\b(LT|lt))-([0-9]{1,6})"
+        [[ $MESSAGE =~ $regexp_message ]]
+        TICKET_ID_MESSAGE="${BASH_REMATCH[0]^^}"
+        if [ -z "$TICKET_ID_MESSAGE" ]; then
+            [[ $CURRENT_BRANCH =~ $regexp_message ]]
+            TICKET_ID="${BASH_REMATCH[0]^^}"
+        fi
+    }
+
+    get_ticket_id
+
+    if [ -n "$TICKET_ID" ]; then 
+        sed -i.bak -e "1s/^/[$TICKET_ID] /" $3
+    fi
+
+}
+
